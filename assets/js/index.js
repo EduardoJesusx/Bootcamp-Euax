dd59fda838d85dc8af3b1cde13eb1bd310027ea9
EduardoJesusx <srduduca@gmail.com>
2022-04-05 13:57:17 -0300

    Finalizando projeto

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+
+addEventListener('submit',()=>{
+  const name = document.getElementById('name').value
+  const mail = document.getElementById('email').value
+  const area = document.getElementById('area').value
+
+  return alert(`WOOOOOOW \n \n Seja Bem-vindo ${name} ao bootcamp da ${area}. \n \n Você se cadastrou com sucesso!`)
+})
